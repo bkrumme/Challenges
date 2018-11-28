544155e4bc0b9d60b14ce79cdc8c4824b0fc4e5b
Brandi Krumme <bkrumme@korewireless.com>
2018-11-28 10:23:40 -0500

    Added challenge from Exercism

---
+++ Challenges/Challenges/Exercism.cs
@@ -0,0 +1,16 @@
+﻿namespace Challenges
+{
+    public static class Exercism
+    {
+        public static bool IsLeapYear(int year)
+        {
+            bool isLeapYear = year % 4 == 0;
+            var divisibleBy100 = year % 100 == 0;
+            if (!divisibleBy100)
+            {
+                return isLeapYear;
+            }
+            return year % 400 == 0;
+        }
+    }
+}
